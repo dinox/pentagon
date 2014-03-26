57140c295b15c135f5b08aa8ebc07f378840876f
Erik Henriksson <erik.henriksson@me.com>
2014-03-26 20:22:03 +0100

    SUB class (untested)

---
+++ sub.h
@@ -0,0 +1,50 @@
+#ifndef SUB
+#define SUB
+
+#include<map>
+#include<set>
+
+using namespace std;
+
+/*
+ * SUB class
+ * All variables are indexed by an int
+ * In the methods we name parameters so that x < y
+ * TODO: Maybe also store y > x so we can get that in
+ *       an efficient way.
+ */
+struct Sub {
+    map<int, set<int>*> *relations;
+
+    // Constructor
+    sub() {
+        relations = new map<int, set<int>*>();
+    }
+
+    // Destructor
+    ~sub() {
+        map<int, set<int>*>::iterator it;
+        for (it = relations.begin(); it != relations.end(); it++)
+            delete it->second;
+        delete relations;
+    }
+
+    // Adds x < y to the map
+    void less_than(int x, int y) {
+        if (!relations->at(x))
+            relations->at(x) = new set<int>();
+        relations.at(x)->insert(y);
+    }
+
+    // Returns true if x < y
+    bool is_less_than(int x, int y) {
+        return relations->at(x)->count(y);
+    }
+
+    // Removes x < y from the map
+    void not_less_than(int x, int y) {
+        relations->at(x) && relations->at(x)->erase(y);
+    }
+};
+
+#endif // SUB
