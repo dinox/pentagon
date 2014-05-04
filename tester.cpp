e95bafd43daa48a623b6b7698a69726ccdcd6741
Boris Peltekov <peltekster@gmail.com>
2014-05-04 18:23:24 +0200

    Benchmarking

--- tester.cpp
+++ tester.cpp
@@ -12,72 +12,6 @@ PentagonDM* dense;
 int nDomains, nVars, nJoins;
 pair<int,int>* joins;
 
-void read_input()
-{
-    scanf("%d %d %d\n", &nDomains, &nVars, &nJoins);
-    
-    stl = new PentagonSTL[nDomains];
-    dense = new PentagonDM[nDomains];
-    joins = new pair<int,int>[nJoins];
-    
-    int i,j,a,b,com,v;
-    
-    for (i=0;i<nDomains;++i) {
-        
-    	stl[i].allocate(nVars);
-        dense[i].allocate(nVars);
-        while (1) {
-            scanf("%d",&com);
-            if (com == 0)
-                break;
-            if (com == 1) {
-                scanf("%d %d %d", &v, &a, &b);
-                stl[i].setIntervalFor(v, Interval(a,b));
-                dense[i].setIntervalFor(v, Interval(a,b));
-            } else if (com == 2) {
-                scanf("%d %d", &a, &b);
-                stl[i].setSubFor(a,b);
-                dense[i].setSubFor(a,b);
-            }
-        }
-    }
-    
-    for (i=0;i<nJoins;++i)
-        scanf("%d %d", &joins[i].first, &joins[i].second);
-}
-/*
-void test_base()
-{
-    Timer t(1);
-    
-    t.start();
-    
-    for (int i=0;i<nOps;++i) {
-        printf("join %d %d\n", ops[i].first, ops[i].second);
-        base[ops[i].first].join(base[ops[i].second]);
-    }
-    
-    t.stop();
-    printf("Base domain perf:\n");
-    t.print_cycles();
-}
-
-void test_DM()
-{
-    Timer t(1);
-    
-    t.start();
-    
-    for (int i=0;i<nOps;++i) {
-        dense[ops[i].first].join(dense[ops[i].second]);
-    }
-    
-    t.stop();
-    printf("DM domain perf:\n");
-    t.print_cycles();
-}
-*/
-
 void printSet(set<int> s)
 {
 	for (set<int>::iterator ii = s.begin(); ii != s.end(); ++ii)
@@ -118,6 +52,80 @@ bool verify(PentagonSTL& a, PentagonDM& b)
 	return true;
 }
 
+void read_input()
+{
+    scanf("%d %d %d\n", &nDomains, &nVars, &nJoins);
+    
+    stl = new PentagonSTL[nDomains];
+    dense = new PentagonDM[nDomains];
+    joins = new pair<int,int>[nJoins];
+    
+    int i,j,a,b,com,v;
+    
+    for (i=0;i<nDomains;++i) {
+        
+    	stl[i].allocate(nVars);
+        dense[i].allocate(nVars);
+        while (1) {
+            scanf("%d",&com);
+            if (com == 0)
+                break;
+            if (com == 1) {
+                scanf("%d %d %d", &v, &a, &b);
+                stl[i].setIntervalFor(v, Interval(a,b));
+                dense[i].setIntervalFor(v, Interval(a,b));
+            } else if (com == 2) {
+                scanf("%d %d", &a, &b);
+                stl[i].setSubFor(a,b);
+                dense[i].setSubFor(a,b);
+            }
+        }
+    }
+    
+    for (i=0;i<nJoins;++i)
+        scanf("%d %d", &joins[i].first, &joins[i].second);
+}
+
+void test_STL()
+{
+    Timer t(1);
+    t.start();
+    
+    for (int i=0;i<nJoins;++i)
+        stl[joins[i].first].join(stl[joins[i].second]);
+    
+    t.stop();
+    printf("PentagonSTL domain perf:\n");
+    t.print_cycles();
+}
+
+void test_DM()
+{
+	Timer t(1);
+	t.start();
+
+	for (int i=0;i<nJoins;++i)
+		dense[joins[i].first].join(dense[joins[i].second]);
+
+	t.stop();
+	printf("PentagonDM domain perf:\n");
+	t.print_cycles();
+}
+
+void verifyAll()
+{
+	bool verified = true;
+
+	for (int i=0;i<nDomains;++i)
+		if (!verify(stl[i], dense[i])) {
+			printf("Verification failed for domain %d\n", i);
+			verified = false;
+		}
+
+	printf("Verification%scompleted!\n", verified ? " " : " NOT ");
+}
+
+/*
 void test_()
 {
 	bool verified = true;
@@ -136,13 +144,15 @@ void test_()
 
     printf("Verification%scompleted!\n", verified ? " " : " NOT ");
 }
+*/
 
 int main(int argc, char** argv)
 {
     read_input();
-    test_();
-    //test_base();
-    //test_DM();
+    //test_();
+    test_STL();
+    test_DM();
+    verifyAll();
     return 0;
 }
 
