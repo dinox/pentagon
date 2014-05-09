598ee0215179561ad1f084b3525efd0b33e0b679
Erik Henriksson <erik.henriksson@me.com>
2014-05-09 22:40:01 +0200

    macros

---
+++ macros.h
@@ -0,0 +1,12 @@
+#ifndef __PENTAGON_MACROS__
+#define __PENTAGON_MACROS__
+
+
+#define L1_SIZE (32)
+#define UK (1)
+#define UI (1)
+#define UJ (1)
+#define SUB_TYPE uint8_t
+#define SUB_BITS (8)
+
+#endif // __PENTAGON_MACROS__
