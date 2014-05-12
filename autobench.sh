fb1a70250c7391b778f1614ec50b5a3a607b01e6
Erik Henriksson <erik.henriksson@me.com>
2014-05-12 19:09:24 +0200

    Use make in autobench

--- autobench.sh
+++ autobench.sh
@@ -4,6 +4,6 @@ for size in 512 1024 2048 4096;
 do
     TT="-DUALL=16 -DL1_SIZE=$size"
     echo $TT
-    g++ -I. -fno-tree-vectorize -funroll-loops -O3 -o benchmark benchmark.cpp $TT
+    make CPPFLAGS="$TT" AVX=1
     ./benchmark benchfile b4096
 done
